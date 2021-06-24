10c239fd0869fc0e8f7c76685bac64959a87614e
Oleshchik <72253617+Oleshchik@users.noreply.github.com>
2021-06-24 10:42:50 +0200

    Update RPS.py

--- RPS.py
+++ RPS.py
@@ -1,27 +1,71 @@
 
 import random
 
-RPS = ["rock", "paper", "scissors"]
-player_choice = int(input("Please make your choise. Type 0 for Rock, 1 for Paper or 2 for Scissors: \n"))
-print(player_choice)
+class RPS:
 
-computer_choice = random.randint(0,2)
-print("Computer chose:")
-print(RPS[computer_choice])
+    def __init__(self):
+        pass
 
-if player_choice >= 3 or player_choice < 0:
-    print("Invalid number, you lose!")
-elif player_choice == computer_choice:
-    print("It's a draw")
-elif player_choice == 0 and computer_choice == 1:
-    print("Computer wins!")    
-elif player_choice == 0 and computer_choice ==2:
-    print("Player wins!")
-elif player_choice == 1 and computer_choice == 0:
-    print("Player wins")
-elif player_choice == 1 and computer_choice == 2:
-    print("Computer wins")
-elif player_choice == 2 and computer_choice == 0:
-    print("Computer wins")
-elif player_choice == 2 and computer_choice == 1:
-    print("Player wins")                
+def elements():
+    pass
+
+def data_input(self):
+    pass
+
+def player_choice(self):
+    pass
+    
+def computer_choice(self):
+    pass
+
+def except_errors():
+    pass
+
+def play_game():
+    pass
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    pass
+
+
+# import sys
+# import random
+# 
+# RPS = ["rock", "paper", "scissors"]
+# 
+# while True:
+#     try:
+#         player_choice = input("Please make your choise. Type 0 for Rock, 1 for Paper or 2 for Scissors (q to quit): \n")
+#         if player_choice.lower() == 'q':
+#             print("Bye")
+#             break
+#         player_choice = int(player_choice)
+#     except ValueError:
+#         print("This is not a number")
+#         continue
+#     if player_choice >= 3 or player_choice < 0:
+#         print("Invalid number, you lose!")
+#     else:
+#         computer_choice = random.randint(0,2)
+# 
+#         print("Player:", RPS[player_choice])
+#         print("Computer:", RPS[computer_choice])
+# 
+#         if player_choice == computer_choice:
+#             print("It's a draw")
+#         elif player_choice == 0 and computer_choice == 1:
+#             print("Computer wins!")
+#         elif player_choice == 0 and computer_choice ==2:
+#             print("Player wins!")
+#         elif player_choice == 1 and computer_choice == 0:
+#             print("Player wins")
+#         elif player_choice == 1 and computer_choice == 2:
+#             print("Computer wins")
+#         elif player_choice == 2 and computer_choice == 0:
+#             print("Computer wins")
+#         elif player_choice == 2 and computer_choice == 1:
+#             print("Player wins")
+             
